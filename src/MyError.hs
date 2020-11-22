8ad6c209cf7de7c9e4ff30e927f2867b6d548a5c
Gaspard Legendre <glegendr@student.42.fr>
2020-11-22 18:49:33 +0100

    Feat: Add color on error

--- src/MyError.hs
+++ src/MyError.hs
@@ -1,16 +1,33 @@
 module MyError
     ( myError
     , exit
+    , myWarning
     ) where
 
 import System.Exit
+import Rainbow
+import Data.Function ((&))
+import Data.Text (pack)
+
+jsonErrToStr :: Bool -> String
+jsonErrToStr True = "JSON "
+jsonErrToStr _ = ""
 
 myError :: Bool -> String -> IO ()
-myError False str = do
-    putStrLn $ "ERROR: " ++ str
+myError b str = do
+    let myErr = lines str
+    putChunkLn $ (chunk $ pack $ jsonErrToStr b ++ "ERROR: " ++ (head myErr)) & fore red & bold
+    case (tail myErr) of
+        [] -> return ()
+        x -> putStrLn $ init $ foldl1 (++) $ map (++ "\n") x
     exitWith (ExitFailure $ length str)
-myError _ str = do
-    putStrLn $ "JSON ERROR: " ++ str
-    exitWith (ExitFailure $ length str)
+
+myWarning :: String -> IO ()
+myWarning str = do
+    let myWarn = lines str
+    putChunkLn $ (chunk $ pack $ "WARNING: " ++ (head myWarn)) & fore yellow & bold
+    case (tail myWarn) of
+        [] -> return ()
+        x -> putStrLn $ init $ foldl1 (++) $ map (++ "\n") x
 
 exit = exitWith ExitSuccess
